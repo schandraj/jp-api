93ec27faef69993bcb55c1fe8546996b3aa8e85b
schandraj <schandrajiwa@gmail.com>
2025-09-14 16:04:51 +0700

    addin allowed origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'https://stg-lms.jadipraktisi.com', 'http://192.168.1.2:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'https://stg-lms.jadipraktisi.com', 'https://jadipraktisi.com', 'https://beta.jadipraktisi.com', 'http://192.168.1.2:3000'],
 
     'allowed_origins_patterns' => [],
 
